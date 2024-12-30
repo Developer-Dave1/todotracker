36e19d2859d86477da3b3336eeaf474c8470fdf5
Developer Dave <116127714+Developer-Dave1@users.noreply.github.com>
2024-12-30 18:23:00 -0500

    updates minor

--- todos.js
+++ todos.js
@@ -9,8 +9,7 @@ const { sortTodoLists, sortTodos } = require("./lib/sort");
 const store = require("connect-loki");
 
 const app = express();
-const host = "localhost";
-const port = 3000;
+const port = process.env.PORT || 3000;
 const LokiStore = store(session);
 
 app.set("views", "./views");
@@ -292,6 +291,6 @@ app.use((err, req, res, _next) => {
 });
 
 // Listener
-app.listen(port, host, () => {
+app.listen(port, () => {
   console.log(`Todos is listening on port ${port} of ${host}!`);
 });
